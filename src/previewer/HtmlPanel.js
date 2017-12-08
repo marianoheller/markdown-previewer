9ff019ac3fd04a54f6a10b8f29ffe57ed169ead3
Mariano Mariano <marianofheller@gmail.com>
2017-12-08 18:53:00 -0300

    Faltan BONUS y se caga desp del test

--- src/previewer/HtmlPanel.js
+++ src/previewer/HtmlPanel.js
@@ -2,16 +2,13 @@ import React, { Component } from 'react';
 
 export default class HtmlPanel extends Component {
     setPreview() {
-        console.log(this.props.currentPreview);
         return { __html: this.props.currentPreview }
     }
 
     render() {
-        
         return (
             <div  className="panel-container">
-                <div id="preview" dangerouslySetInnerHTML={this.setPreview()}>
-                </div>
+                <div id="preview" dangerouslySetInnerHTML={this.setPreview()} />
             </div>
         );
     }
